07c8c21b9847d161a336846bc368afc27b04002b
Daniel Lebu <dalebu@microsoft.com>
2016-04-18 13:32:59 -0700

    Fixed tests.

--- test/cordova.test.ts
+++ test/cordova.test.ts
@@ -27,56 +27,55 @@ suite("VSCode Cordova extension - intellisense and command palette tests", () =>
         return testUtils.removeCordovaComponents("plugin", testProjectPath, ["cordova-plugin-file", "cordova-plugin-whitelist"])
     });
 
-    function checkTypeDefinitions(expectedTypedDefs: string[])
-    {
+    function checkTypeDefinitions(expectedTypedDefs: string[]) {
         let actualTypeDefs = testUtils.enumerateListOfTypeDefinitions(testProjectPath);
         assert.deepEqual(actualTypeDefs, expectedTypedDefs);
     };
 
-    test('Plugin type definitions are installed on activation', () => {
+    test('#Plugin type definitions are installed on activation', () => {
         return Q.delay(10000).then(() => {
             checkTypeDefinitions(["FileSystem.d.ts"]);
         });
     });
 
-    test('Plugin type defintion for a plugin is added upon adding that plugin', () => {
+    test('#Plugin type defintion for a plugin is added upon adding that plugin', () => {
         return testUtils.addCordovaComponents("plugin", testProjectPath, ["cordova-plugin-device"])
-        .then(() => {
-            return Q.delay(10000);
-        }).then(() => {
-            checkTypeDefinitions(["Device.d.ts", "FileSystem.d.ts"]);
-        });
-    });
-
-    test('Plugin type definition for a plugin is removed after removal of that plugin', () => {
-        return testUtils.removeCordovaComponents("plugin", testProjectPath, ["cordova-plugin-device"])
-        .then(() => {
-            return Q.delay(10000);
-        }).then(() => {
-             checkTypeDefinitions(["FileSystem.d.ts"]);
-        });
-    });
-
-    test('Verify that the commands registered by Cordova extension are loaded', () => {
-        return vscode.commands.getCommands(true)
-        .then((results) => {
-            let cordovaCmdsAvailable = results.filter((commandName: string) => {
-                return commandName.indexOf("cordova.") > -1
+            .then(() => {
+                return Q.delay(10000);
+            }).then(() => {
+                checkTypeDefinitions(["Device.d.ts", "FileSystem.d.ts"]);
             });
-            assert.deepEqual(cordovaCmdsAvailable, ["cordova.build", "cordova.run", "cordova.prepare"])
-        });
     });
 
-    test('Execute Commands from the command palette', () => {
-        return testUtils.addCordovaComponents("platform", testProjectPath, ["windows"])
-        .then(() => {
-            return vscode.commands.executeCommand("cordova.build");
-        }).then(() => {
-            return Q.delay(10000);
-        }).then(res => {
-            let appxPackagesParentPath = path.resolve(testProjectPath, "platforms", "windows", "AppPackages");
-            assert.ok(CordovaProjectHelper.existsSync(appxPackagesParentPath));
-            return testUtils.removeCordovaComponents("platform", testProjectPath, ["windows"])
-        });
+    test('#Plugin type definition for a plugin is removed after removal of that plugin', () => {
+        return testUtils.removeCordovaComponents("plugin", testProjectPath, ["cordova-plugin-device"])
+            .then(() => {
+                return Q.delay(10000);
+            }).then(() => {
+                checkTypeDefinitions(["FileSystem.d.ts"]);
+            });
+    });
+
+    test('#Verify that the commands registered by Cordova extension are loaded', () => {
+        return vscode.commands.getCommands(true)
+            .then((results) => {
+                let cordovaCmdsAvailable = results.filter((commandName: string) => {
+                    return commandName.indexOf("cordova.") > -1
+                });
+                assert.deepEqual(cordovaCmdsAvailable, ["cordova.prepare", "cordova.build", "cordova.run", "cordova.simulate"])
+            });
+    });
+
+    test('#Execute Commands from the command palette', () => {
+        return testUtils.addCordovaComponents("platform", testProjectPath, ["android"])
+            .then(() => {
+                return vscode.commands.executeCommand("cordova.build");
+            }).then(() => {
+                return Q.delay(10000);
+            }).then(res => {
+                let androidBuildPath = path.resolve(testProjectPath, "platforms", "android", "build");
+                assert.ok(CordovaProjectHelper.existsSync(androidBuildPath));
+                return testUtils.removeCordovaComponents("platform", testProjectPath, ["android"])
+            });
     });
 });
