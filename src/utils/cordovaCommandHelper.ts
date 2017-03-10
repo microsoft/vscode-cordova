c3915cdef6125b558111c9438eb0dca4425d833b
Nikita Matrosov <v-nimatr@microsoft.com>
2017-03-10 11:08:15 +0300

    Make available to choose platform for ionic commands (build/run/prepare)

--- src/utils/cordovaCommandHelper.ts
+++ src/utils/cordovaCommandHelper.ts
@@ -8,6 +8,7 @@ import * as util from 'util';
 import {window} from 'vscode';
 
 import {TelemetryHelper} from './telemetryHelper';
+import {CordovaProjectHelper} from './cordovaProjectHelper';
 
 export class CordovaCommandHelper {
     private static CORDOVA_CMD_NAME = os.platform() === "win32" ? "cordova.cmd" : "cordova";
@@ -17,10 +18,31 @@ export class CordovaCommandHelper {
     private static CORDOVA_DISPLAY_NAME: string = "Cordova";
     private static IONIC_DISPLAY_NAME: string = "Ionic";
 
+    public static chooseProjectPlatform(projectRoot): Promise<string> {
+        return new Promise((resolve, reject) => {
+            let platforms = CordovaProjectHelper.getInstalledPlatforms(projectRoot);
+
+            if (platforms.length > 1) {
+                resolve(window.showQuickPick(platforms).then((platform) => {
+                    if (!platform) {
+                       throw new Error("Couldn't continue to execute command as long as platform isn't specified");
+                    }
+
+                    return platform;
+                }));
+            } else if (platforms.length === 1) {
+                resolve(platforms[0]);
+            } else {
+                throw new Error("No platform is installed");
+            }
+
+        });
+    }
+
     public static executeCordovaCommand(projectRoot: string, command: string, useIonic: boolean = false) {
-        var telemetryEventName: string = CordovaCommandHelper.CORDOVA_TELEMETRY_EVENT_NAME;
-        var cliCommandName: string = CordovaCommandHelper.CORDOVA_CMD_NAME;
-        var cliDisplayName: string = CordovaCommandHelper.CORDOVA_DISPLAY_NAME;
+        let telemetryEventName: string = CordovaCommandHelper.CORDOVA_TELEMETRY_EVENT_NAME;
+        let cliCommandName: string = CordovaCommandHelper.CORDOVA_CMD_NAME;
+        let cliDisplayName: string = CordovaCommandHelper.CORDOVA_DISPLAY_NAME;
 
         if (useIonic) {
             telemetryEventName = CordovaCommandHelper.IONIC_TELEMETRY_EVENT_NAME;
@@ -28,39 +50,44 @@ export class CordovaCommandHelper {
             cliDisplayName = CordovaCommandHelper.IONIC_DISPLAY_NAME;
         }
 
-        TelemetryHelper.generate(telemetryEventName, (generator) => {
-            generator.add('command', command, false);
-            let outputChannel = window.createOutputChannel("cordova");
-            let commandToExecute = cliCommandName + " " + command;
-            outputChannel.appendLine("########### EXECUTING: " + commandToExecute + " ###########");
-            outputChannel.show();
-            let process = child_process.exec(commandToExecute, { cwd: projectRoot });
+        CordovaCommandHelper.chooseProjectPlatform(projectRoot)
+        .then((platform) => {
+            TelemetryHelper.generate(telemetryEventName, (generator) => {
+                generator.add('command', command, false);
+                let outputChannel = window.createOutputChannel("cordova");
+                let commandToExecute = cliCommandName + " " + command  + " " + platform;
+                outputChannel.appendLine("########### EXECUTING: " + commandToExecute + " ###########");
+                outputChannel.show();
+                let process = child_process.exec(commandToExecute, { cwd: projectRoot });
 
-            let deferred = Q.defer();
-            process.on("error", (err: any) => {
-                // ENOENT error will be thrown if no Cordova.cmd or ionic.cmd is found
-                if (err.code === "ENOENT") {
-                    window.showErrorMessage(util.format("%s not found, please run 'npm install –g %s' to install %s globally", cliDisplayName, cliDisplayName.toLowerCase(), cliDisplayName));
-                }
-                deferred.reject(err);
+                let deferred = Q.defer();
+                process.on("error", (err: any) => {
+                    // ENOENT error will be thrown if no Cordova.cmd or ionic.cmd is found
+                    if (err.code === "ENOENT") {
+                        window.showErrorMessage(util.format("%s not found, please run 'npm install –g %s' to install %s globally", cliDisplayName, cliDisplayName.toLowerCase(), cliDisplayName));
+                    }
+                    deferred.reject(err);
+                });
+
+                process.stderr.on('data', (data: any) => {
+                    outputChannel.append(data);
+                });
+
+                process.stdout.on('data', (data: any) => {
+                    outputChannel.append(data);
+                });
+
+                process.stdout.on("close", (exitCode: number) => {
+                    outputChannel.appendLine("########### FINISHED EXECUTING : " + commandToExecute + " ###########");
+                    deferred.resolve({});
+                });
+
+                return TelemetryHelper.determineProjectTypes(projectRoot)
+                    .then((projectType) => generator.add('projectType', projectType, false))
+                    .then(() => deferred.promise);
             });
-
-            process.stderr.on('data', (data: any) => {
-                outputChannel.append(data);
-            });
-
-            process.stdout.on('data', (data: any) => {
-                outputChannel.append(data);
-            });
-
-            process.stdout.on("close", (exitCode: number) => {
-                outputChannel.appendLine("########### FINISHED EXECUTING : " + commandToExecute + " ###########");
-                deferred.resolve({});
-            });
-
-            return TelemetryHelper.determineProjectTypes(projectRoot)
-                .then((projectType) => generator.add('projectType', projectType, false))
-                .then(() => deferred.promise);
+        }, (err) => {
+            window.showErrorMessage(err.message);
         });
     }
 }
