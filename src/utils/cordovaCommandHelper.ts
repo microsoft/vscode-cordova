04b8d29dbdb6ade8cdd5ca778c20bef2ee040fb3
Yuri Skorokhodov <yuri.skorokhodov@akvelon.com>
2018-10-19 12:24:43 +0300

    Fix for XCode 10 new build system workaround for ionic projects

--- src/utils/cordovaCommandHelper.ts
+++ src/utils/cordovaCommandHelper.ts
@@ -41,8 +41,12 @@ export class CordovaCommandHelper {
                         commandToExecute += ` ${platform}`;
                         // Workaround for dealing with new build system in XCode 10
                         // https://github.com/apache/cordova-ios/issues/407
-                        if (platform === "ios")
+                        if (platform === "ios") {
+                            if (useIonic) {
+                                commandToExecute += " -- ";
+                            }
                             commandToExecute += " --buildFlag='-UseModernBuildSystem=0'";
+                        }
                     }
 
                     const runArgs = CordovaCommandHelper.getRunArguments(projectRoot);
