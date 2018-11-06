ffdc81b83d230c947c0d3a1c0b1221d20fdc4557
Yuri Skorokhodov <yuri.skorokhodov@akvelon.com>
2018-11-06 14:00:30 +0300

    Increase awaiting intervals Add more time for testing

--- test/index.ts
+++ test/index.ts
@@ -8,7 +8,7 @@ let testRunner = require("vscode/lib/testrunner");
 testRunner.configure({
     ui: "tdd", 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
     useColors: true, // colored output from test results
-    timeout: 150000,
+    timeout: 300000,
 });
 
 module.exports = testRunner;
