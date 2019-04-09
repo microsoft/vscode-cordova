e60647ba3520e1f444c467fe592d03b4ffa4481f
Yuri Skorokhodov <v-yuskor@microsoft.com>
2019-04-09 15:18:03 +0300

    Fix tests

--- test/debugger/cordovaPathTransformer.ts
+++ test/debugger/cordovaPathTransformer.ts
@@ -2,14 +2,12 @@
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
 import {CordovaPathTransformer} from "../../src/debugger/cordovaPathTransformer";
-
 import * as path from "path";
-
 import "should";
 
 describe("CordovaPathTransformer", () => {
 
-   it("should correctly convert merges paths for android", () => {
+   it("should correctly convert merges paths for android", async () => {
        let output = "";
        let logger = (message: string) => output += message + "\n";
        let pathTransformer = new CordovaPathTransformer(logger);
@@ -17,8 +15,9 @@ describe("CordovaPathTransformer", () => {
        // __dirname is '/out/test/debugger' so we need to step up three levels to escape completely
        let testapp = path.join(__dirname, "..", "..", "..", "test", "testProject");
        pathTransformer.attach({cwd: testapp, platform: "android", port: 1234});
-
-       pathTransformer.getClientPath("file:///android_asset/www/js/index.js").toLowerCase().should.equal(path.resolve(testapp, "www", "js", "index.js").toLowerCase());
-       pathTransformer.getClientPath("file:///android_asset/www/js/merged.js").toLowerCase().should.equal(path.resolve(testapp, "merges", "android", "js", "merged.js").toLowerCase());
+       let clientIndexPath = await pathTransformer.getClientPath("file:///android_asset/www/js/index.js");
+       let clientMergesPath = await pathTransformer.getClientPath("file:///android_asset/www/js/merged.js");
+       clientIndexPath.toLowerCase().should.equal(path.resolve(testapp, "www", "js", "index.js").toLowerCase());
+       clientMergesPath.toLowerCase().should.equal(path.resolve(testapp, "merges", "android", "js", "merged.js").toLowerCase());
    }).timeout(5000);
 });
