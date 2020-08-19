4f4e475abf15000be4130fa2e262a1ddc2b2d095
Yuri Skorokhodov <v-yuskor@microsoft.com>
2020-08-19 17:55:06 +0300

    Update src/extension/cordovaSessionManager.ts
    Co-authored-by: JiglioNero <43934531+JiglioNero@users.noreply.github.com>

--- src/extension/cordovaSessionManager.ts
+++ src/extension/cordovaSessionManager.ts
@@ -5,7 +5,6 @@ import * as vscode from "vscode";
 import * as Net from "net";
 import { CordovaDebugSession } from "../debugger/cordovaDebugSession";
 
-
 export class CordovaSessionManager implements vscode.DebugAdapterDescriptorFactory {
 
     private servers = new Map<string, Net.Server>();
