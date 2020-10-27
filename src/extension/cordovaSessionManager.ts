211f9a63cd47f26b1b6b1af5e94c68bda9c323e5
RedMickey <mihax2013@yandex.ru>
2020-10-27 11:22:25 +0300

    Add 'cordova.rebuild' command

--- src/extension/cordovaSessionManager.ts
+++ src/extension/cordovaSessionManager.ts
@@ -8,11 +8,16 @@ import { CordovaDebugSession } from "../debugger/cordovaDebugSession";
 
 export class CordovaSessionManager implements vscode.DebugAdapterDescriptorFactory {
 
+    protected readonly cordovaDebuggingFlag = "isCordovaDebugging";
+
     private servers = new Map<string, Net.Server>();
     private connections = new Map<string, Net.Socket>();
+    private cordovaDebugSessions = new Map<string, vscode.DebugSession>();
 
     public createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
         const cordovaDebugSessionId = uuidv4();
+        this.cordovaDebugSessions.set(cordovaDebugSessionId, session);
+        vscode.commands.executeCommand("setContext", this.cordovaDebuggingFlag, true);
 
         const debugServer = Net.createServer(socket => {
             let cordovaDebugSession = new CordovaDebugSession(session, cordovaDebugSessionId, this);
@@ -27,6 +32,11 @@ export class CordovaSessionManager implements vscode.DebugAdapterDescriptorFacto
     }
 
     public terminate(cordovaDebugSessionId: string, forcedStop: boolean = false): void {
+        this.cordovaDebugSessions.delete(cordovaDebugSessionId);
+        if (this.cordovaDebugSessions.size === 0) {
+            vscode.commands.executeCommand("setContext", this.cordovaDebuggingFlag, false);
+        }
+
         this.destroyServer(cordovaDebugSessionId, this.servers.get(cordovaDebugSessionId));
 
         let connection = this.connections.get(cordovaDebugSessionId);
@@ -36,6 +46,16 @@ export class CordovaSessionManager implements vscode.DebugAdapterDescriptorFacto
             }
             this.connections.delete(cordovaDebugSessionId);
         }
+    }
+
+    public getCordovaDebugSessionByProjectRoot(projectRoot: string): vscode.DebugSession | null {
+        for (let session of this.cordovaDebugSessions.values()) {
+            if (session.workspaceFolder.uri.fsPath === projectRoot) {
+                return session;
+            }
+        }
+
+        return null;
     }
 
     public dispose(): void {
