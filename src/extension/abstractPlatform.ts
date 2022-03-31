543f04c597701643a9a19fa6180db99712696b46
JiglioNero <smouk.chayz@gmail.com>
2022-03-31 17:18:14 +0200

    fix

--- src/extension/abstractPlatform.ts
+++ src/extension/abstractPlatform.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
-import * as fs from "fs";
 import { DebugConsoleLogger } from "../debugger/cordovaDebugSession";
 import IonicDevServer from "../utils/ionicDevServer";
 import { IGeneralAttachResult } from "./platformAttachResult";
