40593bdb98a6b91e4f2f7eef272e1274c6b437cc
benjaminbi <42724242+benjaminbi@users.noreply.github.com>
2023-05-19 14:38:49 +0800

    Add localization unit tests and related pipeline task (#861)
    * Add localization unit tests
    Fixes #853
    
    * Add localization unit tests
    Fixes #853
    
    * Add localization unit tests
    Fixes #853
    
    * Add localization unit tests
    Fixes #853
    
    * Update
    
    * Update
    
    * Update
    
    * Update mocha settings
    
    ---------
    
    Co-authored-by: Ezio Li <v-yukl@microsoft.com>

---
+++ test/localization/index.ts
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+import * as path from "path";
+import * as Mocha from "mocha";
+
+export async function run(): Promise<void> {
+    const mocha = new Mocha({
+        ui: "tdd",
+        grep: RegExp("localizationContext"),
+        reporter: "mocha-multi-reporters",
+        reporterOptions: {
+            reporterEnabled: "spec, mocha-junit-reporter",
+            mochaJunitReporterReporterOptions: {
+                mochaFile: path.join(__dirname, "..", "LocalizationTests.xml"),
+            },
+        },
+        color: true,
+    });
+
+    // Register Mocha options
+    return new Promise((resolve, reject) => {
+        mocha.addFile(path.resolve(__dirname, "localization.test.js"));
+
+        try {
+            // Run the mocha test
+            mocha.run((failures: any) => {
+                if (failures > 0) {
+                    reject(new Error(`${failures} tests failed.`));
+                } else {
+                    resolve();
+                }
+            });
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
