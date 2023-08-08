327914318d87a022f7868680878ceb8b887bc128
Ezio Li <v-yukl@microsoft.com>
2023-08-08 05:50:46 +0000

    Add unit test for cordova project in workspace subfolder (#900)

--- test/extension/simulateHelper.test.ts
+++ test/extension/simulateHelper.test.ts
@@ -2,7 +2,11 @@
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
 import * as assert from "assert";
+import * as path from "path";
+import * as fs from "fs";
 import { SimulateHelper } from "../../src/utils/simulateHelper";
+import * as CordovaSimulate from "cordova-simulate";
+import { CordovaProjectHelper } from "../../src/utils/cordovaProjectHelper";
 
 suite("simulateHelper", function () {
     suite("isSimulateTarget", function () {
@@ -19,6 +23,56 @@ suite("simulateHelper", function () {
         test("shouldn't find a target in Simulate targets", () => {
             assert.strictEqual(SimulateHelper.isSimulateTarget("test"), false);
             assert.strictEqual(SimulateHelper.isSimulateTarget("browser"), false);
+        });
+    });
+
+    suite("subfolderVerification", function () {
+        function isSimulateFolderInSubfolder(
+            fsPath: string,
+            workspaceFolderPath: string,
+            simulateOptions: CordovaSimulate.SimulateOptions,
+        ): boolean {
+            const checkPath = path.join(workspaceFolderPath, ".vscode", "simulate");
+            if (fs.existsSync(checkPath)) {
+                simulateOptions.dir = workspaceFolderPath;
+                if (!simulateOptions.simulationpath) {
+                    simulateOptions.simulationpath = checkPath;
+                }
+                return false;
+            } else {
+                simulateOptions.dir = fsPath;
+                if (!simulateOptions.simulationpath) {
+                    simulateOptions.simulationpath = path.join(fsPath, ".vscode", "simulate");
+                }
+                return true;
+            }
+        }
+        test("should get correct path when cordova project in workspace subfolder", () => {
+            let simulateOptions: CordovaSimulate.SimulateOptions = {
+                dir: "",
+                simulationpath: "",
+            };
+            const isSubfolder = isSimulateFolderInSubfolder(
+                path.resolve(__dirname, "../resources/testSubfolderProject/cordovaProject"),
+                path.resolve(__dirname, "../resources/testSubfolderProject"),
+                simulateOptions,
+            );
+
+            assert.strictEqual(isSubfolder, true);
+            assert.strictEqual(
+                simulateOptions.dir,
+                path.resolve(__dirname, "../resources/testSubfolderProject/cordovaProject"),
+            );
+            assert.strictEqual(
+                simulateOptions.simulationpath,
+                path.resolve(
+                    __dirname,
+                    "../resources/testSubfolderProject/cordovaProject/.vscode/simulate",
+                ),
+            );
+
+            const platforms = CordovaProjectHelper.getInstalledPlatforms(simulateOptions.dir);
+            assert.strictEqual(platforms.length, 3);
         });
     });
 });
