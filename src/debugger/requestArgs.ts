65d84450b7d4c8e05f19105f18e627b3fef70b8b
Rodolfo Berwanger Liberado <83656054+RodBerw@users.noreply.github.com>
2024-02-21 23:22:35 -0300

    Update cordova hostname customization support (#944)
    * Update cordova hostname customization support
    
    * Update requestArgs.ts
    
    * Update jsDebugConfigAdapter.ts
    
    * Gulp format
    
    * Revert gulp format
    
    * Update pathMapping hostname value
    
    * Validation argument correction
    
    ---------
    
    Co-authored-by: Rodolfo Liberado (BEYONDSOFT CONSULTING INC) <v-rliberado@microsoft.com>
    Co-authored-by: Ezio Li <v-yukl@microsoft.com>

--- src/debugger/requestArgs.ts
+++ src/debugger/requestArgs.ts
@@ -29,6 +29,7 @@ export interface ICordovaAttachRequestArgs extends DebugProtocol.AttachRequestAr
     attachTimeout?: number;
     simulatorInExternalBrowser?: boolean;
     runtimeVersion?: string;
+    hostname?: string;
 
     // iOS debug properties
     iOSVersion?: string;
